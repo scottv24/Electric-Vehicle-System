3db513e291666f41a1ce09525bb31a7364703418
ajmcbain <71331981+ajmcbain@users.noreply.github.com>
2024-03-04 21:32:42 +0000

    get-data chargers call pulls data from db

--- Implementation/server/routes/Chargers.js
+++ Implementation/server/routes/Chargers.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 const router = express.Router()
 
-router.get('/get-data', (req, res) => {
-    const chargers = ['list', 'of', 'chargers']
+router.get('/get-data', async function (req, res) {
+    const chargers = await prisma.chargingPoint.findMany()
     res.json({ chargers })
 })
 
