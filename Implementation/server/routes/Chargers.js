feeb6aa6aec6db682bb183dbf9672ebe120d9bc2
Scott Valentine <sdv1@hw.ac.uk>
2024-03-05 18:58:38 +0000

    Created query for getting and returning charger data

--- Implementation/server/routes/Chargers.js
+++ Implementation/server/routes/Chargers.js
@@ -4,7 +4,9 @@ const prisma = new PrismaClient()
 const router = express.Router()
 
 router.get('/get-data', async function (req, res) {
-    const chargers = await prisma.chargingPoint.findMany()
+    const chargers = await prisma.location.findMany({
+        include: { chargingPoint: true, queue: true },
+    })
     res.json({ chargers })
 })
 
