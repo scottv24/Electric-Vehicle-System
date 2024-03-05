39f965a39f8294d3b73e03a7a37ad7865aa0dad4
ajmcbain <71331981+ajmcbain@users.noreply.github.com>
2024-03-05 12:29:05 +0000

    clean out index.js

--- Implementation/server/index.js
+++ Implementation/server/index.js
@@ -1,25 +1,12 @@
 const express = require('express')
 const app = express()
-//const PORT = 5000
-//local running port, comment out for deployment
 const PORT = 3000
-
-// const config = require('/config/config.js')
-// const SECRET = 'test'
-// const ngrok_url = '<PUT YOUR NGROK HTTP URL HERE>'
-
-// const cors = require('cors')
-// const bodyParser = require('body-parser')
-// const jwt = require('jsonwebtoken')
-// const nodeMailer = require('nodemailer')
 
 const apiRoute = require('./routes/Api')
 
 app.get('/', (req, res) => {
     res.send('EV Charging Backend :)')
 })
-
-//app.get('/chargers', )
 
 app.use('/api', apiRoute)
 
