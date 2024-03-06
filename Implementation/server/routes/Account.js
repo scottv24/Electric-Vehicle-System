682f60f4adfa67b376c89345c53886dbe9f7298f
ajmcbain <71331981+ajmcbain@users.noreply.github.com>
2024-03-06 00:12:40 +0000

    hard-coded account authentication working with JWT

--- Implementation/server/routes/Account.js
+++ Implementation/server/routes/Account.js
@@ -3,7 +3,9 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const router = express.Router()
 const jwt = require('jsonwebtoken')
-const { sendLoginLink } = require('./mailer') //Email logic
+//const { sendLoginLink } = require('./mailer') //Email logic
+const hbs = require('nodemailer-express-handlebars')
+const nodemailer = require('nodemailer')
 
 //Testing purposes only - DELETE FOR RELEASE
 router.get('/get-data', async function (req, res) {
@@ -23,8 +25,8 @@ router.post('/create-user', async function (req, res) {
     })
 })
 
-router.post('/login', async function (req, res) {
-    const user = await prisma.users.findUnique({
+router.get('/login', async function (req, res) {
+    const user = await prisma.users.findFirst({
         where: {
             email: 'am454@hw.ac.uk',
         },
@@ -32,14 +34,102 @@ router.post('/login', async function (req, res) {
 
     if (user != null) {
         try {
-            //const token = jwt.sign({})
-            //})
-            await sendLoginLink({ email: user, token })
+            const token = jwt.sign(
+                { userId: user.id },
+                process.env.JWT_SECRET,
+                { expiresIn: '1h' },
+            )
+
+            // create the transport channel for emails to be sent through
+            const emailTransport = nodemailer.createTransport({
+                service: 'gmail',
+                secure: true,
+                // sender information
+                auth: {
+                    user: 'mengdevcharger@gmail.com',
+                    pass: process.env.EMAIL_APP_PASSWORD, // app password goes here
+                },
+            })
+
+            // Configure the email transporter to use the email templates
+            emailTransport.use(
+                'compile',
+                hbs({
+                    viewEngine: {
+                        extname: '.handlebars',
+                        layoutsDir: 'email_templates/',
+                        defaultLayout: false,
+                        partialsDir: 'email_templates',
+                    },
+                    viewPath: 'email_templates',
+                    extName: '.handlebars',
+                }),
+            )
+
+            try {
+                // example email
+                const emailOptions = {
+                    from: 'mengdevcharger@gmail.com',
+                    to: user.email, //test email here
+                    subject: 'loginEmail',
+                    template: 'loginEmail',
+                    context: {
+                        user: user.email,
+                        link: `http://localhost:3000/api/account/verify-user?token=${token}`,
+                    },
+                }
+
+                emailTransport.sendMail(emailOptions, (err, res) => {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        console.log('Email Sent: ' + res.response)
+                    }
+                })
+            } catch (err) {
+                console.error('Error: ', err)
+                //res.status(500).send('Internal Error')
+            }
+
+            //await sendLoginLink({ email: user, token })
         } catch (error) {
             return res.send('Error logging in, please try again')
         }
     }
     res.send('Check your email to finish logging in')
+    //res.send(user.email)
 })
+
+router.get('/verify-user', async function (req, res) {
+    const token = req.query.token
+    if (token == null) return res.sendStatus(401)
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+        const user = await prisma.users.findFirst({
+            where: {
+                id: decodedToken.userId,
+            },
+        })
+        res.send('Authenticated, welcome')
+    } catch (error) {
+        res.sendStatus(401)
+    }
+})
+
+// crypto.subtle
+//     .generateKey(
+//         {
+//             name: 'HMAC',
+//             hash: { name: 'SHA-256' },
+//         },
+//         true,
+//         ['sign', 'verify'],
+//     )
+//     .then((key) => {
+//         crypto.subtle.exportKey('jwk', key).then((exported) => {
+//             console.log(exported.k)
+//         })
+//     })
 
 module.exports = router
