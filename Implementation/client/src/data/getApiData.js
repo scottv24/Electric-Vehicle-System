c376e5cf291d4c9523ebe3d423eb80aa9b016c57
Scott Valentine <sdv1@hw.ac.uk>
2024-03-19 17:05:58 +0000

    Fixed API endpoint for local testing

--- Implementation/client/src/data/getApiData.js
+++ Implementation/client/src/data/getApiData.js
@@ -5,9 +5,11 @@ export default async function getApiData(route) {
         `${window.location.origin.toString()}/hwcharging/api/${route}/get-data`
     )
     try {
+        //`${window.location.origin.toString()}/hwcharging/api/${route}/get-data`
+        const url = `http://localhost:3000/api/${route}/get-data`
         const resp = await axios.get(
             //`http://localhost:3000/api/${route}/get-data`,
-            `${window.location.origin.toString()}/hwcharging/api/${route}/get-data`,
+            url,
             {
                 headers: {
                     'Access-Control-Allow-Origin': '*',
