461301fe7fbafb2e3ed5209140de4b025ff0b1f2
kd41 <kyled073@gmail.com>
2024-03-21 20:48:31 +0000

    Basic Templating for Emails

--- Implementation/server/index.js
+++ Implementation/server/index.js
@@ -32,6 +32,9 @@ app.get('/', (req, res) => {
 app.use(express.json())
 app.use('/api', apiRoute)
 
+/**********************************
+ *      EMAILING FUNCTIONALITIES
+ **********************************/
 // create the transport channel for emails to be sent through
 const emailTransport = nodemailer.createTransport({
     service: 'gmail',
@@ -58,21 +61,24 @@ emailTransport.use(
     }),
 )
 
-// route test email page
-app.get('/test-email', async (req, res) => {
-    try {
-        // example email
-        const emailOptions = {
-            from: 'mengdevcharger@gmail.com',
-            to: '', //test email here
-            subject: 'testEmail',
-            template: 'testEmail',
-            context: {
-                user: '',
-                zone: '',
-            },
-        }
+// Function to compose and send an email
+function createEmail(recipient, subject, template, context) {
+    var emailOptions =  {
+        from: 'mengdevcharger@gmail.com',
+        to: recipient, //test email here
+        subject: subject,
+        template: template,
+        context: context,
+        // all emails have an attachement which is the logo
+        attachments: [{
+            filename: 'testLogo.png',
+            path: __dirname + '/email_templates/testLogo.png',
+            cid: 'testLogo' //same cid value as in the html img src
+        }]
+    }
 
+    // attempt to send the email
+    try {
         emailTransport.sendMail(emailOptions, (err, res) => {
             if (err) {
                 console.log(err)
@@ -84,6 +90,54 @@ app.get('/test-email', async (req, res) => {
         console.error('Error: ', err)
         res.status(500).send('Internal Error')
     }
+}
+
+/**********************************
+ *      EMAIL CONTEXTS
+ *      Each email only cares about certain information
+ *      The following functions sets the information to 
+ *      be used in the email
+ **********************************/
+let testEmailContext = {
+    title: "Test Email!"
+}
+let chargeFinishedContext = (recipient, zone, queueSize) => {
+    return {
+        title: "Your Vehicle is Fully Charged!",
+        user: recipient,
+        zone: zone,
+        queueSize: queueSize
+    }
+}
+let nextQueueContext = (recipient, zone, queueSize) => {
+    return {
+        title: "You Are Next in Queue!",
+        user: recipient,
+        zone: zone,
+    }
+}
+let signInContext = (recipient, zone) => {
+    return {
+        title: "Thank You For Using",
+        user: recipient,
+        zone: zone
+    }
+}
+
+/**********************************
+ *      TEST ROUTES FOR EMAIL
+ **********************************/
+app.get('/test-email', async () => {
+    createEmail('', 'test email', 'testEmail', testEmailContext);
+})
+app.get('/charge-finish-email', async () => {
+    createEmail('', 'test email', 'ChargeFinished', chargeFinishedContext('Adam', 'Robotarium', 5));
+})
+app.get('/next-queue-email', async () => {
+    createEmail('', 'test email', 'NextQueue', nextQueueContext('Eve', 'Oriam', 2));
+})
+app.get('/park-sign-in-email', async () => {
+    createEmail('', 'test email', 'ParkingSignIn', signInContext('Mr President', 'Park J'));
 })
 
 app.listen(PORT, () => {
