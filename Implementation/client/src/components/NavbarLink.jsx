41b44a971d40700eb5168bf2926e46e6b3828cbe
cc164 <cc164@hw.ac.uk>
2024-03-22 07:39:10 +0000

    Navbar for mobile

--- Implementation/client/src/components/NavbarLink.jsx
+++ Implementation/client/src/components/NavbarLink.jsx
@@ -1,6 +1,26 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-export default function NavbarLink({ field, active }) {
+ 
+export default function NavbarLink({ field, active, mobile }) {
+    if (mobile) {
+        return (
+            <div
+                className={` hover:font-bold font-semibold text-nav-primary md:w-full
+         w-16 md:rounded-r-none rounded-full mt-2 aspect-square h-2/3 ${
+             active ? 'bg-nav-selected text-nav-selected' : 'hover:bg-nav-hover'
+         }`}
+            >
+                <a
+                    href={field.path}
+                    className="py-4 px-6 flex justify-start items-center w-full"
+                >
+                    {field.icon && (
+                        <FontAwesomeIcon icon={field.icon} className="mr-4" />
+                    )}
+                    <p className="md:block hidden">{field.name}</p>
+                </a>
+            </div>
+        )
+    }
     return (
         <li
             className={` hover:font-bold font-semibold text-nav-primary md:w-full
